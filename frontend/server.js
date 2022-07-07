25817118749894841fcca6cfef91f86439dc8317
Cody Denniston <dennistonc4817@gmail.com>
2022-07-07 18:09:52 -0500

    deleted package.json

---
+++ frontend/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const server = express();
+const port = process.env.PORT || 3000;
+
+
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`);
+ });
